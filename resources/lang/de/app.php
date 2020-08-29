008c2316e3efcab14b395b7445c13c7b7a451513
Daniel Brendel <dbrendel1988@gmail.com>
2020-08-29 22:59:02 +0200

    Better cookie consent text

--- resources/lang/de/app.php
+++ resources/lang/de/app.php
@@ -43,7 +43,7 @@
         'account_confirmed_ok' => 'Your account has successfully be confirmed. You may now log in.',
         'workspace_link' => 'Your workspace link: ',
         'ok' => 'Ok',
-        'cookie_consent' => 'This service stores and reads cookies. Also it uses Google Analytics to record traffic. By using this service you agree automatically to that usage.',
+        'cookie_consent' => 'This service is using cookies. By continuing browsing here you agree to that usage.',
         'workspace_cancel' => 'Cancel Workspace',
         'workspace_cancel_btn' => 'Cancel and delete my data',
         'workspace_cancel_confirm' => 'Are you sure you want to proceed? This cannot be undone! All your data will be erased.',
