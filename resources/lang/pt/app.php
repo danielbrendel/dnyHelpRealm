b5f9c0cebaeb0bf6636eb1ea611ba4a2869cb4ed
Daniel Brendel <dbrendel1988@gmail.com>
2021-01-06 00:04:30 +0100

    Update dependencies

--- resources/lang/pt/app.php
+++ resources/lang/pt/app.php
@@ -2,7 +2,7 @@
     /*
         HelpRealm (dnyHelpRealm) developed by Daniel Brendel
 
-        (C) 2019 - 2020 by Daniel Brendel
+        (C) 2019 - 2021 by Daniel Brendel
 
         Version: 1.0
         Contact: dbrendel1988<at>gmail<dot>com
